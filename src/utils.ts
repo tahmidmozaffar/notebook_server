9d6ef801bbdf4c40532686611559f91ed3be8162
Md Tahmid Mozaffar <tahmid1989@gmail.com>
2022-11-29 01:36:31 -0500

    fixed build

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,7 @@ export const split = (str: string, index: number) => {
 
   return result;
 }
+
+export const toUpperFirstChar = (name: string) => {
+  return name[0].toUpperCase() + name.slice(1);
+}
