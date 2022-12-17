c33485776c022e576984b4edc63cb635f4dd9ba8
Md Tahmid Mozaffar <tahmid1989@gmail.com>
2022-12-17 07:13:41 -0500

    test added for login method

--- src/controllers/__tests__/authController.spec.ts
+++ src/controllers/__tests__/authController.spec.ts
@@ -2,8 +2,11 @@ import { User } from "../../models/user.model";
 import userService from "../../services/user.service";
 import { mockRequest, mockResponse } from "../../test-utils/mockUtils";
 import authControllers from "../auth.controller";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 describe("Auth controller tests", () => {
+
   describe("Signup method", () => {
 
     it("when no exception happens, createUser will be called, 201 code and success message will be sent", async () => {
@@ -51,6 +54,73 @@ describe("Auth controller tests", () => {
 
     afterEach(() => {
       jest.clearAllMocks();
-    })
+    });
+  });
+
+  describe("Login method", () => {
+    it("when exception happens, 500 code and failure message will be sent", async () => {
+      const req = mockRequest({});
+      const res = mockResponse();
+      jest.spyOn(userService, 'getUserByUsername').mockResolvedValue(Promise.reject());
+
+      await authControllers.login(req, res);
+
+      expect(res.status).toBeCalledWith(500);
+      expect(res.json).toBeCalledWith({ message: "Something went wrong. Try again later" });
+    });
+
+    it("userservice.getUserByUsername will be called with username from request body", async () => {
+
+      const req = mockRequest({ username: "anyusername" });
+      const res = mockResponse();
+      const spy = jest.spyOn(userService, 'getUserByUsername');
+
+      await authControllers.login(req, res);
+
+      expect(spy).toBeCalledWith("anyusername");
+    });
+
+    it("when no user exists with the username, 404 code and 'User does not exist' message will be sent", async () => {
+      const req = mockRequest({ username: "anyusername" });
+      const res = mockResponse();
+      jest.spyOn(userService, 'getUserByUsername').mockResolvedValue(null);
+
+      await authControllers.login(req, res);
+
+      expect(res.status).toBeCalledWith(404);
+      expect(res.json).toBeCalledWith({ message: "User does not exist" });
+    });
+
+    it("when called with valid username and password, 200 code and success message with token will be sent", async () => {
+      const req = mockRequest({ username: "validusername", password: "validpassword" });
+      const res = mockResponse();
+
+      jest.spyOn(userService, 'getUserByUsername').mockResolvedValue(Promise.resolve({} as User));
+      bcrypt.compare = jest.fn().mockResolvedValue(true);
+      jwt.sign = jest.fn().mockReturnValue("generatedtoken");
+
+      await authControllers.login(req, res);
+
+      expect(res.status).toBeCalledWith(200);
+      expect(res.json).toBeCalledWith({ message: "Successfully logged in", token: "generatedtoken" });
+    });
+
+    it("when called with wrong password, 401 code and failure message will be sent ", async () => {
+      const req = mockRequest({ username: "validusername", password: "validpassword" });
+      const res = mockResponse();
+
+      jest.spyOn(userService, 'getUserByUsername').mockResolvedValue(Promise.resolve({} as User));
+      bcrypt.compare = jest.fn().mockResolvedValue(false);
+
+      await authControllers.login(req, res);
+
+      expect(res.status).toBeCalledWith(401);
+      expect(res.json).toBeCalledWith({ message: "Password is incorrect" });
+
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
   });
 });
