a24f79ff1d1b247c0282cb621d08007ee5f13872
Md Tahmid Mozaffar <tahmid1989@gmail.com>
2022-12-17 06:09:22 -0500

    tests added, minor changes in test running setup

--- src/controllers/__tests__/authController.spec.ts
+++ src/controllers/__tests__/authController.spec.ts
@@ -6,16 +6,51 @@ import authControllers from "../auth.controller";
 describe("Auth controller tests", () => {
   describe("Signup method", () => {
 
-    it("when there is no exception, createUser will be called, 201 code and success message will be sent", async () => {
-      const req = mockRequest();
+    it("when no exception happens, createUser will be called, 201 code and success message will be sent", async () => {
+      const req = mockRequest({});
       const res = mockResponse();
-
       jest.spyOn(userService, 'createUser').mockResolvedValue({} as User);
+
       await authControllers.signup(req, res);
 
       expect(userService.createUser).toBeCalledTimes(1);
-      expect(res.status).toBeCalledWith(201)
-      expect(res.send).toBeCalledWith("User is created successfully")
+      expect(res.status).toBeCalledWith(201);
+      expect(res.send).toBeCalledWith("User is created successfully");
     });
+
+    it("when excepton happens, 500 code and failure message will be sent", async () => {
+      const req = mockRequest({});
+      const res = mockResponse();
+      jest.spyOn(userService, 'createUser').mockResolvedValue(Promise.reject());
+
+      await authControllers.signup(req, res);
+      expect(userService.createUser).toBeCalledTimes(1);
+      expect(res.status).not.toBeCalledWith(201);
+      expect(res.status).toBeCalledWith(500);
+      expect(res.send).toBeCalledWith({ message: "Something went wrong. Please try again." });
+    });
+
+    it("createUser of userService will be call with body parameter", async () => {
+      const name = "any name";
+      const username = "anyusername";
+      const password = "anypassword";
+      const email = "any@email.com";
+
+      const req = mockRequest({
+        name,
+        username,
+        password,
+        email,
+      });
+      const res = mockResponse();
+
+      const spy = jest.spyOn(userService, "createUser");
+      await authControllers.signup(req, res);
+      expect(spy).toHaveBeenCalledWith(name, username, password, email);
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    })
   });
 });
--- src/test-utils/mockUtils.ts
+++ src/test-utils/mockUtils.ts
@@ -4,16 +4,19 @@ export const mockRequest = (body?: any): Request => ({
   body,
 } as Request);
 
+
+type ResponseOb = {
+  status?: any,
+  send?: any,
+  json?: any,
+}
+
 export const mockResponse = (): Response => {
-  type ResponseOb = {
-    status?: any,
-    send?: any,
-    json?: any,
-  }
   const res: ResponseOb = {};
 
   res.status = jest.fn().mockReturnValue(res);
   res.send = jest.fn().mockReturnValue(res);
   res.json = jest.fn().mockReturnValue(res);
+  
   return res as Response;
 };
