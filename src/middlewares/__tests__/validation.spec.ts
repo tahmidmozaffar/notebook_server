5d5fa01c8f82ac24367e8583c32b177fa3815e93
Md Tahmid Mozaffar <tahmid1989@gmail.com>
2022-12-20 02:52:57 -0500

    more tests added for validation

--- src/middlewares/__tests__/validation.spec.ts
+++ src/middlewares/__tests__/validation.spec.ts
@@ -1,40 +1,114 @@
 import { mockRequest, mockResponse } from "../../test-utils/mockUtils";
-import { fieldValidation } from "../validation";
+import { confirmPasswordValidation, emailValidation, fieldValidation } from "../validation";
 
 describe("validation middleware tests", () => {
+  let res: any;
+  let nextSpy: any;
+
+  beforeEach(() => {
+    res = mockResponse();
+    nextSpy = jest.fn();
+  });
+
   describe("fieldValidation method", () => {
-    let res: any;
-    let spyNext: any;
-
-    beforeEach(() => {
-      res = mockResponse();
-      spyNext = jest.fn();
-    });
-
     it("when email is given, next method will be called", () => {
       const func = fieldValidation('email');
       const req = mockRequest({ body: { email: "any@email.com" } });
-      const res = mockResponse();
-      const spyNext = jest.fn();
 
-      func(req, res, spyNext);
+      func(req, res, nextSpy);
 
       expect(res.status).not.toBeCalledWith(422);
       expect(res.send).not.toBeCalledWith({ message: "Email is required" });
-      expect(spyNext).toBeCalledTimes(1);
+      expect(nextSpy).toBeCalledTimes(1);
     });
 
     it("when email is not given, next method will not be called", () => {
       const func = fieldValidation('email');
       const req = mockRequest({ body: {} });
-      const res = mockResponse();
-      const spyNext = jest.fn();
 
-      func(req, res, spyNext);
+      func(req, res, nextSpy);
 
       expect(res.status).toBeCalledWith(422);
       expect(res.send).toBeCalledWith({ message: "Email is required" });
-      expect(spyNext).not.toBeCalled();
+      expect(nextSpy).not.toBeCalled();
+    });
+  });
+
+  describe("confirmPasswordValidation method", () => {
+    it("when new password and confirm password are same, next method will be called", () => {
+      const req = mockRequest({ body: { newPassword: "password", confirmPassword: "password" } });
+
+      confirmPasswordValidation(req, res, nextSpy);
+
+      expect(res.status).not.toBeCalledWith(422);
+      expect(res.send).not.toBeCalledWith({ message: "Password does not match" });
+      expect(nextSpy).toBeCalledTimes(1);
+    });
+
+    it("when new password and confirm password are different, next method will not be called", () => {
+      const req = mockRequest({ body: { newPassword: "password", confirmPassword: "otherpassword" } });
+
+      confirmPasswordValidation(req, res, nextSpy);
+
+      expect(res.status).toBeCalledWith(422);
+      expect(res.send).toBeCalledWith({ message: "Password does not match" });
+      expect(nextSpy).not.toBeCalled();
+    });
+  });
+
+  describe("emailValidation method", () => {
+    it("when email is not given, 422 code will be sent", () => {
+      const req = mockRequest({ body: {} });
+
+      emailValidation(req, res, nextSpy);
+
+      expect(res.status).toBeCalledWith(422);
+      expect(res.send).toBeCalledWith({ message: "Email is required" });
+      expect(nextSpy).not.toBeCalled();
+    });
+
+    it("when email is valid, next method will be called", () => {
+
+      const emails = [
+        'any@email.com',
+        '12asd@email.com',
+        'asdasd12@email.com',
+      ];
+
+      emails.forEach(email => {
+        const req = mockRequest({ body: { email } });
+
+        emailValidation(req, res, nextSpy);
+
+        expect(res.status).not.toBeCalledWith(422);
+        expect(res.send).not.toBeCalledWith({ message: "Email is invalid" });
+        expect(nextSpy).toBeCalledTimes(1);
+
+        jest.clearAllMocks();
+      });
+
+    });
+
+    it("when email is invalid, next method will not be called", () => {
+
+      const emails = [
+        'any @email.com',
+        '12asd@email. com',
+        'asdasd12@ email.com',
+        'invalid.com',
+        'any@emailcom',
+        'any@email.',
+      ];
+
+      emails.forEach(email => {
+        const req = mockRequest({ body: { email } });
+
+        emailValidation(req, res, nextSpy);
+
+        expect(res.status).toBeCalledWith(422);
+        expect(res.send).toBeCalledWith({ message: "Email is invalid" });
+        expect(nextSpy).not.toBeCalled();
+      });
     });
   });
 });
